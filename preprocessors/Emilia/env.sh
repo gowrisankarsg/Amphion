0381d08aad58fdd43a866d053d6a63946431b2d1
Gowrisankar S <114475480+gowrisankarsg@users.noreply.github.com>
2026-05-16 13:18:27 +0530

    Update env.sh

--- preprocessors/Emilia/env.sh
+++ preprocessors/Emilia/env.sh
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-conda install ffmpeg -y
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+# conda install ffmpeg -y
+# conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
 pip install -r requirements.txt
 pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
